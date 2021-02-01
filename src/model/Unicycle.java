54c06ae47e4b993bdc4fc2a5f94c3ae515f4e47f
rick5757 <77943138+rick5757@users.noreply.github.com>
2021-02-01 13:35:48 -0600

    Add files via upload

---
+++ src/model/Unicycle.java
@@ -0,0 +1,63 @@
+package model;
+
+// Added by Rick Mendenhall
+
+public class Unicycle {
+	
+	private String make;
+	private String model;
+	private int year;
+	
+	/**
+	 * Default No-Arg Constructor
+	 */
+	public Unicycle() {
+		super();
+	}
+
+	/**
+	 * @param make
+	 * @param model
+	 * @param year
+	 */
+	public Unicycle(String make, String model, int year) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "Unicycle [make=" + make + ", model=" + model + ", year=" + year + "]";
+	}
+	
+	public String makeNoise() {
+		return "OOPS! Ouch!";
+	}
+	
+}
